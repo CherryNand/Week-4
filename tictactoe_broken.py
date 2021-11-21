fecc782a3d05aaa70a3c2ab425a4ad814c10f61e
CherryNand <abhinanddsunil@gmail.com>
2021-11-21 13:24:06 +0400

    the code has been corrected

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -67,7 +67,7 @@ def add_piece(game, player, row, column):
     row: 0-index row
     column: 0-index column
     """
-    game[row][column+1] = player
+    game[row][column] = player #removed the '+1' after column
     return game
 
 def check_space_empty(game, row, column):
@@ -77,7 +77,7 @@ def convert_input_to_coordinate(user_input):
     return user_input - 1
 
 def switch_player(player):
-    if player = 1:
+    if player == 1: #changed "=" to "=="
         return 2
     else:
         return 1
@@ -98,11 +98,11 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available: #added colon
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
-        game = add_piece(game, player, row, column)
+            available=check_space_empty(game,row,column)
+        game = add_piece(game, player, row, column)#added the column argument to the check_space_empty() function
         display_game(game)
         player = switch_player(player)
 #        winner = check_winner(game)
